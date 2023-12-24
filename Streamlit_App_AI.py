a6e0dbada432c36a7795f39d36912636459fd9c1
XavierMv <xaviermv96@gmail.com>
2023-12-24 18:09:26 -0500

    omit matplotlib library

--- Streamlit_App_AI.py
+++ Streamlit_App_AI.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import pandas as pd
 import numpy as np 
-import matplotlib.pyplot as plt
+#import matplotlib.pyplot as plt
 from sklearn.datasets import make_regression
 from sklearn.linear_model import Ridge, Lasso
 from sklearn.metrics import mean_absolute_error, mean_squared_error
