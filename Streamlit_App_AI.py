407ae6301c0622e850b4581257fa42424b5811a9
XavierMv <xaviermv96@gmail.com>
2023-12-23 23:21:47 -0500

    modified Streamlit_App_AI.py for homepage edition
    This commit introduces the initial version of the Streamlit app.
    It includes the main script on the homepage tab

--- Streamlit_App_AI.py
+++ Streamlit_App_AI.py
@@ -5,8 +5,48 @@ import numpy as np
 
 def homepage():
     st.title("Welcome to My Streamlit App")
-    st.write("In this streamlit App I want to give you a cheatsheet of how to use Python for AI applications in an easy way.")
-    # Add more content about what users can find on your app
+    st.write(
+        "Hello and welcome to my Streamlit app! My goal is to provide you with "
+        "basic knowledge on how to use Python for AI and business applications."
+    )
+
+    st.markdown(
+        """
+        ## Professional Background
+        I bring a strong background in credit risk analysis and data science, with hands-on experience
+        in developing impactful solutions for leading financial and consulting organizations.
+
+        ### Santander - Credit Risk Intern
+        As a Credit Risk intern at Santander, I played a key role in developing time series models to estimate
+        credit default. This involved leveraging advanced statistical techniques to analyze historical data and
+        predict future credit events. My work contributed to enhancing the bank's ability to make informed
+        decisions in managing credit risk.
+
+        ### Accenture - Data Scientist
+        In my role as a Data Scientist at Accenture, I worked on a variety of projects spanning revenue growth
+        management (RGM), data monetization strategies, customer analytics, and smart spend strategies. I provided
+        valuable consulting advice for AI implementations across different industries. My responsibilities included
+        developing models to optimize RGM, crafting data monetization strategies, and providing insights through
+        advanced analytics.
+
+        My experience at Accenture involved collaborating with cross-functional teams, interpreting complex data
+        sets, and delivering actionable insights to clients. I also contributed to the development of innovative AI
+        solutions tailored to the specific needs of diverse industries.
+
+        I am passionate about the intersection of data science and business strategy, and I am excited to share my
+        knowledge with you through this Streamlit app.
+        """
+    )
+
+    st.markdown(
+        """
+        ## Connect with Me
+        If you have any questions or would like to connect, feel free to reach out to me on LinkedIn:
+        """
+    )
+
+    st.markdown("[Connect with Me on LinkedIn](https://www.linkedin.com/in/xaviermagana/)")
+
 
 def basic_python_page():
     st.title("Basic Python Commands with Pandas and NumPy")
