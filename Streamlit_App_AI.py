8fdf1875b1c598c6cf82450f2a57624cfb9dd690
XavierMv <xaviermv96@gmail.com>
2023-12-23 22:55:13 -0500

    Add Streamlit_App_AI.py for initial version
    This commit introduces the initial version of the Streamlit app.
    It includes the main script (Streamlit_App_AI.py) with basic functionality,
    such as displaying a title and a welcome message.

---
+++ Streamlit_App_AI.py
@@ -0,0 +1,10 @@
+import streamlit as st
+import pandas as pd
+import numpy as np 
+
+def main():
+    st.title("My Streamlit App")
+    st.write("Hello, this is a simple Streamlit app!")
+
+if __name__ == "__main__":
+    main()
